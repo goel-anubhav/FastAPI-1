345bd078c54f0ec22225db03f19566575678a462
goel-anubhav <anubhavgoyal12062001@gmail.com>
2024-12-26 12:04:59 +0530

    BASIC CRUD FAST API DONE

--- api/main.py
+++ api/main.py
@@ -1,19 +1,44 @@
 from fastapi import FastAPI
-from .routes import users
-
+from api.models import Todo
 
 app = FastAPI()
+todos = []
 
-# app.include_router(users.router)
+# Get the home page
+@app.get("/")
+async def home():
+    return{"msg":"Hello"}
 
-student = {
-    1: {
-        "Name": "Anubhav",
-        "age":"23",
-        "class":"Developer"
-    }
-}
+# Fetch all the todos
+@app.get("/todos")
+async def get_todos():
+    return{"todos": todos}
 
-@app.get("/get_students/{student}")
-def get_student(student_id: int):
-    return student[student_id]
+# Fetch Particular todo
+@app.get("/todos/{todo_id}")
+async def get_one_todo(todo_id:int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return{"todo": todo}     
+    return{"message":"No todo found"}
+
+# Create Todos
+@app.post("/todos")
+async def create_todos(todo: Todo):
+    todos.append(todo)
+    return{"Success": "Todo Has Been Added"}
+
+# Delete Todos
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo: Todo):
+    todos.remove(todo)
+    return{"message": "Todo is deleted"}
+# Update the data
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id:int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            return{"message":"Todo is updated"}
+
--- api/schemas.py
+++
@@ -1,9 +0,0 @@
-import motor.motor_asyncio
-from bson import objectid
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_URL"))
-
-db = client.blog_api
